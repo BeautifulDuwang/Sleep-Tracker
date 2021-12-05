7d04a68a8c610e67c4363c22efe2cb783726f791
BeautifulDuwang <dhayek92@gmail.com>
2021-12-05 11:55:05 +0200

    Commit

---
+++ app/src/main/java/lau/project/sleeptracker/MainPage.java
@@ -0,0 +1,38 @@
+package lau.project.sleeptracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainPage extends AppCompatActivity {
+
+    Button slpTracker, slpTips, profile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        slpTracker = findViewById(R.id.btn1);
+        slpTips = findViewById(R.id.btn2);
+        profile = findViewById(R.id.btn3);
+    }
+
+    public void goToTracker(View view){
+        Intent intent = new Intent(getApplicationContext(), TrackerPage.class);
+        startActivity(intent);
+    }
+
+//    public void goToTips(View view){
+//        Intent intent = new Intent(getApplicationContext(), TipsPage.class);
+//        startActivity(intent);
+//    }
+//
+//    public void goToProfile(View view){
+//        Intent intent = new Intent(getApplicationContext(), ProfilePage.class);
+//        startActivity(intent);
+//    }
+}
---
+++ app/src/main/java/lau/project/sleeptracker/TrackerPage.java
@@ -0,0 +1,14 @@
+package lau.project.sleeptracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrackerPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracker_page);
+    }
+}
