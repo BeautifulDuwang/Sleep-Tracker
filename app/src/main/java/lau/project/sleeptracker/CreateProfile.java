4cf5d75a84f4a5a1d95535f9dd843e6982e35e90
BeautifulDuwang <dhayek92@gmail.com>
2021-12-12 20:39:04 +0200

    Commit

--- app/src/main/java/lau/project/sleeptracker/CreateProfile.java
+++ app/src/main/java/lau/project/sleeptracker/CreateProfile.java
@@ -3,12 +3,28 @@ package lau.project.sleeptracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CreateProfile extends AppCompatActivity {
+
+    EditText enterName, enterAge, enterWeight;
+    boolean userMade = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
+
+        enterName = findViewById(R.id.enterName);
+        enterAge = findViewById(R.id.enterAge);
+        enterWeight = findViewById(R.id.enterWeight);
+
+        String name, age, weight;
+
+        name = enterName.getText().toString();
+        age = enterAge.getText().toString();
+        weight = enterWeight.getText().toString();
+
+        userMade = true;
     }
 }
