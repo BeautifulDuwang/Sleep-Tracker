c8820b61ea39cac50f0f0066c720c194de116e4a
BeautifulDuwang <dhayek92@gmail.com>
2021-12-12 21:27:07 +0200

    Commit

--- app/src/main/java/lau/project/sleeptracker/ProfilePage.java
+++ app/src/main/java/lau/project/sleeptracker/ProfilePage.java
@@ -1,15 +1,25 @@
 package lau.project.sleeptracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class ProfilePage extends AppCompatActivity {
 
+    private static final int PICK_FROM_GALLERY = 1;
+
     String name, age, weight;
     EditText editName, editAge, editWeight;
+    Button image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,7 @@ public class ProfilePage extends AppCompatActivity {
         editName = findViewById(R.id.enterName);
         editAge = findViewById(R.id.enterAge);
         editWeight = findViewById(R.id.enterWeight);
+        image = findViewById(R.id.addImage);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null){
@@ -30,6 +41,35 @@ public class ProfilePage extends AppCompatActivity {
         }
 
 
+    }
+
+    public void setImage(View view){
+        try {
+            if (ActivityCompat.checkSelfPermission(ProfilePage.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(ProfilePage.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PICK_FROM_GALLERY);
+            } else {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, PICK_FROM_GALLERY);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PICK_FROM_GALLERY:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(galleryIntent, PICK_FROM_GALLERY);
+                } else {
+                    //do something like displaying a message that he didn`t allow the app to access gallery and you wont be able to let him select from gallery
+                }
+                break;
+        }
     }
 
     public void fillFields(){
